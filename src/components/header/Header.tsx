03a7c1d5509f8b6f5de396cb8020e30f89e1cb75
Sergiy <sergpyatko@gmail.com>
2024-09-26 13:20:59 +0300

    Added header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,50 @@
+import Container from "../container/Container";
+
+import "./Header.scss";
+
+import discount_cross from "../../assets/icons/Vector.svg";
+import shop_logo from "../../assets/images/SHOP.CO.png";
+import search from "../../assets/icons/Search.svg";
+import cart from "../../assets/icons/Cart.svg";
+import person from "../../assets/icons/Person.svg";
+import arrow_down from "../../assets/icons/Arrow_down.svg";
 
 const Header = () => {
-    return(
-        <header>
-
-        </header>
+    return (
+        <>
+            <div className="dicount-background">
+                <Container>
+                    <div className="discount-main-wrapper">
+                        <p>Sign up and get 20% off to your first order. <span className="discount-main-wrapper__sign">Sign Up Now</span></p>
+                        <img src={discount_cross} alt="cross" className="discount-main-wrapper__cross"/>
+                    </div>
+                </Container>
+            </div>
+            <div className="header-background">
+                <Container>
+                    <header>
+                        <img src={shop_logo} alt="Shop.com logo"/>
+                        <div className="header-links">
+                            <span className="header-links__link-arrow">
+                                <a href="#">Shop</a>
+                                <img src={arrow_down}/>
+                            </span>
+                            <a href="#">On Sale</a>
+                            <a href="#">New Arrivals</a>
+                            <a href="#">Brands</a>
+                        </div>
+                        <div className="header-search-bar">
+                            <img src={search} alt="search"/>
+                            <input type="text" placeholder="Search for products..."/>
+                        </div>
+                        <div className="header-icons">
+                            <img src={cart} alt="cart"/>
+                            <img src={person} alt="person"/>
+                        </div>
+                    </header>
+                </Container>
+            </div>
+        </>
     );
 }
 
